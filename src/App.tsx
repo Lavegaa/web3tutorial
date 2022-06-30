b29c3fab205607afe4333ee8add24bc455444143
lavegaa <cksgh405@naver.com>
2022-06-30 22:20:41 +0900

    ✨ : 기본 세팅 중, alias문제 발생

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SamplePage from '@pages/SamplePage';
+
+const App = () => {
+  return <SamplePage />;
+};
+
+export default App;
---
+++ src/pages/SamplePage/SamplePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Sample from '@components/Sample';
+
+const SamplePage = () => {
+  return <Sample />;
+};
+
+export default SamplePage;
